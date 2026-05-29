1e4e566967a12ef07dedbb5300e9f0ba183a97ce
khiri <abdelohabk@gmail.com>
2026-05-29 12:39:17 +0100

    adding certificates

--- scripts/generate-certs.sh
+++ scripts/generate-certs.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+export MSYS_NO_PATHCONV=1
+export MSYS2_ARG_CONV_EXCL="*"
 set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -16,7 +19,7 @@ mkdir -p certs
 openssl genrsa -out certs/ca.key 4096
 openssl req -new -x509 -days 3650 -key certs/ca.key \
   -out certs/ca.crt \
-  -subj "/CN=internal-ca/O=MyApp"
+  -subj "//CN=internal-ca/O=MyApp"
 
 # 2. Generate cert for each service
 for SERVICE in gateway users-service media-service products-service eureka-server; do
@@ -32,7 +35,7 @@ for SERVICE in gateway users-service media-service products-service eureka-serve
 EOF
   openssl req -new -key certs/$SERVICE.key \
     -out certs/$SERVICE.csr \
-    -subj "/CN=$SERVICE/O=MyApp"
+    -subj "//CN=$SERVICE/O=MyApp"
   openssl x509 -req -days 365 \
     -in certs/$SERVICE.csr \
     -CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial \
