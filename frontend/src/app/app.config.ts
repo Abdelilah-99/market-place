af7887a4816e11c24a843ca6151be7b134810b8c
abdelilah <bouchikhiabdelilah0@gmail.com>
2026-06-18 00:42:23 +0100

    enhancing the style

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi, withInterceptors } from '@angular/common/http';
 import { ApiPrefixInterceptor } from './core/interceptors/api-prefix-interceptor';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 
@@ -15,6 +15,11 @@ export const appConfig: ApplicationConfig = {
     },
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(withInterceptors([authInterceptor])),
-    provideRouter(routes)
+    provideRouter(
+      routes,
+      withViewTransitions({
+        skipInitialTransition: true
+      })
+    )
   ]
 };
